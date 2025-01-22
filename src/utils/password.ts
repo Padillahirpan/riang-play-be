9059b8c82951ba1748e3bc5d811f2110a2614550
Padillahirpan <padillahirpan8@gmail.com>
2025-01-22 17:52:08 +0700

    feat: add password utils

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -2,8 +2,6 @@ import { hash, argon2id, verify } from "argon2";
 
 
 export const hashPassword = async(password: string) => {
-   // const hashedPassword = await Bun.password.hash(password);
-
    const hashedPassword = await hash(password, {type: argon2id});
    return hashedPassword;
 }
@@ -12,8 +10,6 @@ export const verifyPassword = async(
    hashPassword: string,
    requestPassword: string
 ) => {
-   // const isPasswordMatch = await Bun.password.verify(requestPassword, hashPassword);
-
    const isPasswordMatch = await verify(requestPassword, hashPassword);
    console.log("this verify password:",isPasswordMatch);
 
