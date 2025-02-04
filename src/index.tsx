9f1521e5fa7746cb194cc92273e22348ea03e899
Padillahirpan <padillahirpan8@gmail.com>
2025-02-04 22:01:33 +0700

    feat: add hono swagger

--- src/index.tsx
+++ src/index.tsx
@@ -3,33 +3,43 @@ import { userRoute } from './features/user/route';
 import { authRoute } from './features/auth/route';
 import { productRoute } from './features/product/route';
 import { cartRoute } from './features/cart/route';
+import { swaggerUI } from '@hono/swagger-ui';
+import { cors } from 'hono/cors';
 
 const app = new OpenAPIHono();
-
 app.route('api/auth', authRoute);
 app.route('api/users', userRoute);
 app.route('api/products', productRoute);
 app.route('api/cart', cartRoute);
-app.get('/', (c) => {
-   return c.html(
-      <html lang="en">
-         <head>
-            <meta charset="UTF-8" />
-            <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1.0"
-            />
-            <title>Welcome to MusicHits2000 REST API</title>
-            <meta name="description" content="Web API about movies" />
-            <script src="https://cdn.tailwindcss.com"></script>
-         </head>
-         <body>
-            <div>
-               {' '}
-               <h1>Welcome to REST API</h1>{' '}
-            </div>
-         </body>
-      </html>
-   );
-});
+app.doc31('/docs', {
+   openapi: '3.0.0',
+   info: {
+      version: '1.0.0',
+      title: 'Riang-Play Rest API',
+      description: 'Riang-Play rest API provides access to simple e-commerce.',
+   },
+})
+   .get('/ui', swaggerUI({ url: '/docs' }))
+   .get('/', (c) => {
+      return c.html(
+         <html lang="en">
+            <head>
+               <meta charset="UTF-8" />
+               <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1.0"
+               />
+               <title>Welcome to Riang-Play REST API</title>
+               <meta name="description" content="Web API about movies" />
+               <script src="https://cdn.tailwindcss.com"></script>
+            </head>
+            <body>
+               <div>
+                  {' '}
+                  <h1>Welcome to Riang-Play REST API</h1>{' '}
+               </div>
+            </body>
+         </html>
+      );
+   });
 export default app;
